4665c3b947ce37caa13ca963233d194b5795b1f3
Muhammad Talha <mtalhaf@gmail.com>
2017-02-23 20:01:37 +0000

    ran catkin_make against the directory for the first time

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/ros-raspberry-pi-node/src
+# Build directory: /home/ubuntu/ros-raspberry-pi-node/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)

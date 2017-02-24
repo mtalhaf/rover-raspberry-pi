cdf4001aaef39a420c29cf5cfebe78f380e7496c
mtalhaf93 <mtalhaf@gmail.com>
2017-02-24 19:13:15 +0000

    changed lcd api to get message from url paramter and display that to user.

--- src/arduino_android_connection/scripts/lcd/lcdApiService.py
+++ src/arduino_android_connection/scripts/lcd/lcdApiService.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask, request
 from printMessage import lcd_printMessage
 app = Flask(__name__)
 
 @app.route('/v1.0/lcd/print', methods=['GET'])
 def printMessageOnLcd():
-	lcd_printMessage("Hello from api")
+        message = request.args.get('message')
+	lcd_printMessage(message)
+	return "message printed"
 
 if __name__ == '__main__':
 	app.run(debug=True)

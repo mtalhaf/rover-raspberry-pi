66c40a7e58186024d5c67d33f5b83d183ce30490
mtalhaf93 <mtalhaf@gmail.com>
2017-02-26 12:50:44 +0000

    updated movement service api to use blueprint.

--- src/arduino_android_connection/scripts/movement/movementApiService.py
+++ src/arduino_android_connection/scripts/movement/movementApiService.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-from flask import Flask, request
+from flask import Flask, request, Blueprint
 #from printMessage import lcd_printMessage
-app = Flask(__name__)
 
-@app.route('/v1.0/movement/moveforward', methods=['GET'])
+movement_api = Blueprint('movement_api', __name__)
+
+@movement_api.route('/v1.0/movement/moveforward', methods=['GET'])
 def moveRobotForward():
         #message = request.args.get('message')
 	#lcd_printMessage(message)
 	return "robot moving"
-
-if __name__ == '__main__':
-	app.run(host='0.0.0.0')
 

17f33c4655c50d3f25798ee6f1e4db53b4625789
mtalhaf93 <mtalhaf@gmail.com>
2017-02-26 13:27:26 +0000

    added backward turn left and turn right routes to api.

--- src/arduino_android_connection/scripts/movement/movementApiService.py
+++ src/arduino_android_connection/scripts/movement/movementApiService.py
@@ -7,5 +7,20 @@ movement_api = Blueprint('movement_api', __name__)
 @movement_api.route('/v1.0/movement/moveforward', methods=['GET'])
 def moveRobotForward():
         movement_moveForward()
-	return "robot moving"
+	return "robot moving forward"
+
+@movement_api.route('/v1.0/movement/movebackward', methods=['GET'])
+def moveRobotBackward():
+        movement_moveBackward()
+	return "robot moving backward"
+
+@movement_api.route('/v1.0/movement/turnleft', methods=['GET'])
+def turnRobotLeft():
+        movement_turnLeft()
+	return "robot turning left"
+
+@movement_api.route('/v1.0/movement/turnright', methods=['GET'])
+def turnRobotRight():
+        movement_turnRight()
+	return "robot trning right"
 

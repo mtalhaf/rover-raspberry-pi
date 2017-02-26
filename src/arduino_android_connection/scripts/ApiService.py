675eae55affb80e676c1098918752a178921b4ff
mtalhaf93 <mtalhaf@gmail.com>
2017-02-26 12:51:08 +0000

    added high level api service file which will call the routes on multiple services.

---
+++ src/arduino_android_connection/scripts/ApiService.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from flask import Flask, request
+from movement/movementApiService import movement_api
+from lcd/lcdApiService import lcd_api
+
+app = Flask(__name__)
+app.register_blueprint(movement_api)
+app.register_blueprint(lcd_api)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
+
